dbb8c560c342417a068241e59bd29e943a62e15d
Glomberg <Bazz@bk.ru>
2015-09-29 03:11:50 +0400

    Верстка главной готова

--- www/js/script.js
+++ www/js/script.js
@@ -1,1 +1,9 @@
-﻿
+﻿$(document).ready(function(){
+	$("a.scroll").on("click", function(y){
+		y.preventDefault();
+		href = $(this).attr("href");
+		if($(href).length != 0) {
+			$("body,html").animate({ scrollTop: $(href).offset().top }, 1000);
+		}
+	});
+});
